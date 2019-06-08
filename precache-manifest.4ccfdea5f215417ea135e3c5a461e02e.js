6b4313706876432e879ee47c61ded9b434cf8de1
AlexGithubbb <31776215+AlexGithubbb@users.noreply.github.com>
2019-06-08 14:43:10 -0400

    Updates

---
+++ precache-manifest.4ccfdea5f215417ea135e3c5a461e02e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robopals/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "85ca4ef05e834621cc56",
+    "url": "/robopals/static/js/runtime~main.1d0bf8ea.js"
+  },
+  {
+    "revision": "a59eade61cd2a4a8dca9",
+    "url": "/robopals/static/js/main.fb18d471.chunk.js"
+  },
+  {
+    "revision": "7df182c2ab0e923feb20",
+    "url": "/robopals/static/js/2.85996f6a.chunk.js"
+  },
+  {
+    "revision": "a59eade61cd2a4a8dca9",
+    "url": "/robopals/static/css/main.50ed1d70.chunk.css"
+  },
+  {
+    "revision": "7df182c2ab0e923feb20",
+    "url": "/robopals/static/css/2.0fbe2c6f.chunk.css"
+  },
+  {
+    "revision": "1464f50579beac65acedf58e5f91ea88",
+    "url": "/robopals/index.html"
+  }
+];
